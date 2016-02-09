a2dfec78a9f61dad94986b1f4c72c079a3db659e
JonathanH5 <Jonathan@Hasenburg.de>
2016-02-09 11:40:12 +0100

    Adjusted sockets to new styling idea, missing: rules and sensor data

--- meinHeim/meinHeim.py
+++ meinHeim/meinHeim.py
@@ -27,36 +27,36 @@ class Rules(object):
 
 	class Generale_Rule():
 		__metaclass__ = abc.ABCMeta
-	
+
 		keep_alive = False
 		thread = None
 		tname = ""
-		
+
 		def activate_rule(self):
 			self.keep_alive = True
 			if self.thread != None: # check whether not initialized
 				if self.thread.isAlive(): # check whether was still alive
 					cherrypy.log(self.tname + " was still alive!")
 					return
-		
+
 			# not initialized or dead, does not matter
 			cherrypy.log("Activated Rule " + self.tname + ".")
 			self.thread = Thread(name=self.tname, target=self.rule)
 			self.thread.setDaemon(True)
 			self.thread.start()
-			
+
 		def deactivate_rule(self):
 			cherrypy.log("Rule " + self.tname + "will not be kept alive.")
 			self.keep_alive = False
-			
+
 		@abc.abstractmethod
-		def rule(self):	
+		def rule(self):
 			"This method is abstract"
-		
+
 		def __init__(self, tname):
 			self.tname = tname
-			
-	class Watering_Rule(Generale_Rule):	
+
+	class Watering_Rule(Generale_Rule):
 		def rule(self):
 			while self.keep_alive:
 				now = datetime.datetime.now()
@@ -65,24 +65,24 @@ class Rules(object):
 					#tinkerforge_connection.switch_socket("nXN", 31, 1, 1)
 					time.sleep(60)
 					cherrypy.log("It is " + str(now.hour) + ":" + str(now.minute) + ", stopped watering.")
-					#tinkerforge_connection.switch_socket("nXN", 31, 1, 0)	
+					#tinkerforge_connection.switch_socket("nXN", 31, 1, 0)
 				time.sleep(50)
 			cherrypy.log(self.tname + " was no longer kept alive.")
-			
-	class Balkon_Rule(Generale_Rule):	
+
+	class Balkon_Rule(Generale_Rule):
 		def rule(self):
 			while self.keep_alive:
 				now = datetime.datetime.now()
 				if (now.hour == 17 and now.minute == 0):
 					cherrypy.log("It is " + str(now.hour) + ":" + str(now.minute) + ", activated Balkonbeleuchtung.")
-					tinkerforgeConnection.switch_socket("nXN", 50, 1, 1)	
+					tinkerforgeConnection.switch_socket("nXN", 50, 1, 1)
 				if (now.hour == 22 and now.minute == 0):
 					cherrypy.log("It is " + str(now.hour) + ":" + str(now.minute) + ", deactivated Balkonbeleuchtung.")
 					tinkerforgeConnection.switch_socket("nXN", 50, 1, 0)
 				time.sleep(50)
 			cherrypy.log(self.tname + " was no longer kept alive.")
-			
-	class Desklamp_Rule(Generale_Rule):	
+
+	class Desklamp_Rule(Generale_Rule):
 		def rule(self):
 			send_on = False
 			while self.keep_alive:
@@ -91,21 +91,21 @@ class Rules(object):
 					send_on = True
 				elif (tinkerforge_connection.get_distance("iTm") > 1500 or tinkerforge_connection.get_illuminance("amm") > 30) and send_on == True:
 					tinkerforge_connection.switch_socket("nXN", 30, 3, 0)
-					send_on = False		
+					send_on = False
 				time.sleep(10)
 			cherrypy.log(self.tname + " was no longer kept alive.")
-	
+
 
 	# define public variables for all rules
 	watering_rule = None
 	balkon_rule = None
 	desklamp_rule = None
-			
+
 	def __init__(self):
 		self.watering_rule = Rules.Watering_Rule("Watering Rule")
 		self.balkon_rule = Rules.Balkon_Rule("Balkon Rule")
 		self.desklamp_rule = Rules.Desklamp_Rule("Desklamp Rule")
-		
+
 ##########################################################################################
 #The Webserver
 ##########################################################################################
@@ -125,24 +125,50 @@ class Webserver(object):
 
 	# buttons to switch normal sockets + shutdown dimmer
 	class Socket():
-	
+
+		@cherrypy.expose
+		def list(self):
+			list = self.create_socket_entry("Schreibtischlampe", 30, 3)
+			list += self.create_socket_entry("Hintergrundbeleuchtung", 30, 2)
+			list += self.create_socket_entry("Laterne", 31, 2)
+			#list += self.create_socket_entry("Nachtlicht", 25, 1)
+			list += self.create_socket_entry("Balkonbeleuchtung", 50, 1)
+			return list
+
 		@cherrypy.expose
 		def nXN(self, address=-1, unit=-1, state=-1):
 			if (address == -1 or unit == -1 or state == -1):
-				return ("Invalid information: " + str(address) 
-					+ ", " + str(unit) + ", " + str(state))	
+				return ("Invalid information: " + str(address)
+					+ ", " + str(unit) + ", " + str(state))
 			tinkerforge_connection.switch_socket(
-				"nXN", 
-				int(address), 
-				int(unit), 
+				"nXN",
+				int(address),
+				int(unit),
 				int(state))
 			return "Tried to switch socket"
-	
+
+		def create_socket_entry(self, name, address, unit):
+			return (
+			"<div class='large-12 columns'>" +
+				"<div class='panel clearfix'>" +
+					"<p>" + name + " (" + str(address) + "_" + str(unit) + ", nXN)</p>" +
+					"<button class='small success right button' "+
+					"onclick='console.log($.ajax(\"/socket/nXN?address=" + str(address) +
+					"&unit=" + str(unit) +
+					"&state=1\"));' style='width:100px'>An</button>" +
+					"<button class='small alert right button' "+
+					"onclick='console.log($.ajax(\"/socket/nXN?address=" + str(address) +
+					"&unit=" + str(unit) +
+					"&state=0\"));' style='width:100px'>Aus</button>"
+				"</div>" +
+			"</div>"
+			)
+
 	# buttons that control dimmer
 	class Dimmer():
 		# TODO does not work yet
 		value_25_1 = 10
-	
+
 		@cherrypy.expose
 		def button_nXN_25_1_increase(self):
 			if self.value_25_1 < 15:
@@ -150,7 +176,7 @@ class Webserver(object):
 				tinkerforge_connection.dimm_socket("nXN", 25, 1, self.value_25_1)
 				return "Neuer Dimmwert 25_1 = " + str(self.value_25_1)
 			return "Dimmwert war schon bei 15"
-	
+
 		@cherrypy.expose
 		def button_nXN_25_1_decrease(self):
 			if self.value_25_1 > 0:
@@ -158,53 +184,53 @@ class Webserver(object):
 				tinkerforge_connection.dimm_socket("nXN", 25, 1, self.value_25_1)
 				return "Neuer Dimmwert 25_1 = " + str(self.value_25_1)
 			return "Dimmwert war schon bei 0"
-	
+
 		@cherrypy.expose
 		def button_nXN_25_1_off(self):
 			tinkerforge_connection.switch_socket("nXN", 25, 1, 0)
-			return "Deaktiviere 25_0"	
-	
+			return "Deaktiviere 25_0"
+
 	# buttons that control the rules
 	class Rule():
 		@cherrypy.expose
 		def watering_rule_on(self):
 			rules.watering_rule.activate_rule()
 			return "Watering Rule activated"
-	
+
 		@cherrypy.expose
 		def watering_rule_off(self):
 			rules.watering_rule.keep_alive = False
 			return "Test Rule deactivated"
-			
+
 		@cherrypy.expose
 		def watering_rule_status(self):
 			if rules.watering_rule.keep_alive:
 				return "<a href='.' onclick='return $.ajax(\"/rule/watering_rule_off\");'>Aktiv</a>"
 			else:
 				return "<a href='.' onclick='return $.ajax(\"/rule/watering_rule_on\");'>Deaktiv</a>"
-			
+
 		@cherrypy.expose
 		def balkon_rule_on(self):
 			rules.balkon_rule.activate_rule()
 			return "Balkon Rule activated"
-	
+
 		@cherrypy.expose
 		def balkon_rule_off(self):
 			rules.balkon_rule.keep_alive = False
 			return "Balkon Rule deactivated"
-	
+
 		@cherrypy.expose
 		def balkon_rule_status(self):
 			if rules.balkon_rule.keep_alive:
 				return "<a href='.' onclick='return $.ajax(\"/rule/balkon_rule_off\");'>Aktiv</a>"
 			else:
 				return "<a href='.' onclick='return $.ajax(\"/rule/balkon_rule_on\");'>Deaktiv</a>"
-	
+
 		@cherrypy.expose
 		def desk_lamb_rule_on(self):
 			rules.desklamp_rule.activate_rule()
 			return "Desk Lamb Rule activated"
-	
+
 		@cherrypy.expose
 		def desk_lamb_rule_off(self):
 			rules.desklamp_rule.keep_alive = False
@@ -215,8 +241,8 @@ class Webserver(object):
 			if rules.desklamp_rule.keep_alive:
 				return "<a href='.' onclick='return $.ajax(\"/rule/desk_lamb_rule_off\");'>Aktiv</a>"
 			else:
-				return "<a href='.' onclick='return $.ajax(\"/rule/desk_lamb_rule_on\");'>Deaktiv</a>"	
-		
+				return "<a href='.' onclick='return $.ajax(\"/rule/desk_lamb_rule_on\");'>Deaktiv</a>"
+
 	# queries that provide additional informationen
 	class AdditionalInformation():
 		@cherrypy.expose
@@ -227,15 +253,15 @@ class Webserver(object):
 			for key in tinkerforge_connection.current_entries:
 				string += "<li>"+key+" ("+tinkerforge_connection.current_entries[key]+")</li>"
 			return string
-	
+
 		@cherrypy.expose
 		def amm_illuminance(self):
 			return str(tinkerforge_connection.get_illuminance("amm"))
-		
+
 		@cherrypy.expose
 		def iTm_distance(self):
 			return str(tinkerforge_connection.get_distance("iTm"))
-		
+
 		@cherrypy.expose
 		def bvg(self):
 			array = bvg.call()
@@ -267,14 +293,14 @@ if __name__ == '__main__':
 			'tools.staticdir.index': 'index.html'
 		}
 	}
-	
+
 	# start all needed modules
 	tinkerforge_connection = TinkerforgeConnection()
 	bvg = BVG("Seesener Str. (Berlin)", limit=4)
-	
+
 	# load the rules
 	rules = Rules()
-	
+
 	# start the webserver
 	cherrypy.tree.mount(Webserver().socket, '/socket')
 	cherrypy.tree.mount(Webserver().dimmer, '/dimmer')
--- meinHeim/website/js/frontend.js
+++ meinHeim/website/js/frontend.js
@@ -1,23 +1,14 @@
 $(document).ready(function() {
-	
-	//Button pressed
-	$(".button").click(function(e) {
-		console.log("Button clicked");
-		//TODO Dimmer
-		$.ajax({
-			type: "GET",
-			url: "/socket/nXN",
-			data:{
-				address: $(this).attr("address"), 
-				unit: $(this).attr("unit"), 
-				state: $(this).attr("state")
-			}
-		})
-		.done(function(string) {
-			console.log(string);
-		});
+
+	//Load socket list
+	$.ajax({
+		type: "GET",
+		url: "/socket/list",
+	})
+	.done(function(string) {
+		$("#sockets").replaceWith(string);
 	});
-	
+
 	//Load bvg information
 	$.ajax({
 		type: "GET",
@@ -26,7 +17,7 @@ $(document).ready(function() {
 	.done(function(string) {
 		$("#information_bvg").replaceWith(string);
 	});
-	
+
 	//Load connected devices
 	$.ajax({
 		type: "GET",
@@ -35,7 +26,7 @@ $(document).ready(function() {
 	.done(function(string) {
 		$("#information_connected_devices").replaceWith(string);
 	});
-	
+
 	//Load illuminance from BrickletAmbientLight(amm)
 	$.ajax({
 		type: "GET",
@@ -44,7 +35,7 @@ $(document).ready(function() {
 	.done(function(string) {
 		$("#information_amm_illuminance").replaceWith('<li id="information_amm_illuminance">Aktuelle Helligkeit (amm): ' + string + ' Lux</li>');
 	});
-	
+
 	//Load distance from BrickletDistanceUS(iTm)
 	$.ajax({
 		type: "GET",
@@ -53,7 +44,7 @@ $(document).ready(function() {
 	.done(function(string) {
 		$("#information_iTm_distance").replaceWith('<li id="information_iTm_distance">Aktuelle Entfernung (iTm): ' + string + '</li>');
 	});
-	
+
 	//Load status of watering rule
 	$.ajax({
 		type: "GET",
@@ -62,7 +53,7 @@ $(document).ready(function() {
 	.done(function(string) {
 		$("#watering_rule_status").replaceWith('<li id="watering_rule_status">Bewässerungsregel (9 + 19 Uhr): ' + string + '</li>');
 	});
-	
+
 	//Load status of balkon rule
 	$.ajax({
 		type: "GET",
@@ -71,7 +62,7 @@ $(document).ready(function() {
 	.done(function(string) {
 		$("#balkon_rule_status").replaceWith('<li id="balkon_rule_status">Balkonbeleuchtungsregel (17 - 22 Uhr): ' + string + '</li>');
 	});
-	
+
 	//Load status of desk lamp rule
 	$.ajax({
 		type: "GET",
@@ -80,5 +71,5 @@ $(document).ready(function() {
 	.done(function(string) {
 		$("#desk_lamb_rule_status").replaceWith('<li id="desk_lamb_rule_status">Schreibtischlampenregel: ' + string + '</li>');
 	});
-	
+
 });
